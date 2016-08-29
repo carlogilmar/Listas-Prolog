bfa395a0a8a5205c6e609a34e3f333ca5527c419
carlogilmar <carlogilmar12@gmail.com>
2016-08-28 20:09:57 -0500

    #mostrar_lista Imprime la lista vertical

--- menu_listas.pl
+++ menu_listas.pl
@@ -1,13 +1,31 @@
+lista_a([1,2,3]).
+b([a,b,c]).
+
 menu :- repeat,nl,
     write('Bienvenido al primer programa en prolog para listas'),nl,
-    write('   1.the number of hello  '),nl,
+    write('1. Mostrar lista en horizontal y vertical'),nl,
     write('               5.- para salir           '),nl,
-    write('enter your choice:'),nl,
+    write('Coloca el numero de opcion seguido de un punto final-->'),nl,nl,
     read(Choice), Choice>0, Choice<6,
     doit(Choice).
 
 doit(1):-
-    write('Bienvenido a la opcion 1').
+    write('Bienvenido a la opcion 1'),nl,
+    write('Mostrando lista A en vertical:'), nl,
+    escribe([1,2,3,4]).
+    #write('Mostrando en horizontal lista A:'), nl.
+    #hori([1,2,3,4]), nl.
+
+
+#------------------------------------escribe en vertical
+escribe([]).
+escribe([X|Y]):-
+		write(X),nl,
+		escribe(Y).
+
+
+#-------------------------------------escribe en horizontal
+
 
 doit(2):-
 	write('Bienvenido a la opcion 2').
