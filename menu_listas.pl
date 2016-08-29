a24666f6f6102b28b4c77f7174ca2b48ae94e078
carlogilmar <carlogilmar12@gmail.com>
2016-08-28 20:49:29 -0500

    #insertar_elemento Agregue la concatenacion, y añadir al final de la lista, falta añadir al inicio

--- menu_listas.pl
+++ menu_listas.pl
@@ -1,10 +1,8 @@
-lista_a([1,2,3]).
-b([a,b,c]).
-
 menu :- repeat,nl,nl,
     write('Bienvenido al primer programa en prolog para listas'),nl,
     write('1. Mostrar lista A en horizontal'),nl,
     write('2. Mostrar la lista A en vertical'),nl,
+    write('3. Agregar un elemento al final'),nl,
     write('               5.- para salir           '),nl,
     write('Coloca el numero de opcion seguido de un punto final-->'),nl,nl,
     read(Choice), Choice>0, Choice<6,
@@ -14,26 +12,36 @@ doit(1):-
     write('Bienvenido a la opcion 1'),nl,
     write('Mostrando lista A en vertical:'), nl,
     escribe([1,2,3,4]).
-#------------------------------------escribe en vertical
-escribe([]).
-escribe([X|Y]):-
-		write(X),nl,
-		escribe(Y).
+
+		escribe([]).
+		escribe([X|Y]):-
+				write(X),nl,
+				escribe(Y).
 
 doit(2):-
 	write('Bienvenido al complemento de la opc 2'),nl,
 	write('Mostrando lista A en horizontal: '), nl,
 	hori([1,2,3,4]).
 
-#-------------------------------------escribe en horizontal
-hori([]).
-hori([X|Y]):-
-		write(X),
-		hori(Y).
+		hori([]).
+		hori([X|Y]):-
+				write(X),
+				hori(Y).
 
 
 doit(3):-
-	write('Bienvenido a la opcion 3').
+	write('Bienvenido a la opcion 3'),nl,
+	write('Agregar elemento al final de la lista A [1,2,3,4]'),nl,
+	write('Dame un elemento -->'),
+	read(X),
+	añadir_por_detras([1,2,3,4],X,Lista_nueva), 
+	hori(Lista_nueva).
+
+			concatenar([],L,L).
+		    concatenar([A|B], X, [A|C]):- concatenar(B,X,C).
+
+		    añadir_por_detras([],[E],[E]).
+		    añadir_por_detras([X|Xs],E,[X|Xz]):-concatenar(Xs,[E],Xz).
 
 doit(4):-
 	write('Bienvenido a la opcion 4').
