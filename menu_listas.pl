9873ccebf8d453049b7ab91d9b3b1856b0c14187
carlogilmar <carlogilmar12@gmail.com>
2016-08-28 20:29:20 -0500

    #mostrar_lista Se agrega un nuevo punto en el menu como opcion para mostrar la lista horizontal, no pude hacerlo en uno

--- menu_listas.pl
+++ menu_listas.pl
@@ -1,9 +1,10 @@
 lista_a([1,2,3]).
 b([a,b,c]).
 
-menu :- repeat,nl,
+menu :- repeat,nl,nl,
     write('Bienvenido al primer programa en prolog para listas'),nl,
-    write('1. Mostrar lista en horizontal y vertical'),nl,
+    write('1. Mostrar lista A en horizontal'),nl,
+    write('2. Mostrar la lista A en vertical'),nl,
     write('               5.- para salir           '),nl,
     write('Coloca el numero de opcion seguido de un punto final-->'),nl,nl,
     read(Choice), Choice>0, Choice<6,
@@ -13,22 +14,23 @@ doit(1):-
     write('Bienvenido a la opcion 1'),nl,
     write('Mostrando lista A en vertical:'), nl,
     escribe([1,2,3,4]).
-    #write('Mostrando en horizontal lista A:'), nl.
-    #hori([1,2,3,4]), nl.
-
-
 #------------------------------------escribe en vertical
 escribe([]).
 escribe([X|Y]):-
 		write(X),nl,
 		escribe(Y).
 
+doit(2):-
+	write('Bienvenido al complemento de la opc 2'),nl,
+	write('Mostrando lista A en horizontal: '), nl,
+	hori([1,2,3,4]).
 
 #-------------------------------------escribe en horizontal
+hori([]).
+hori([X|Y]):-
+		write(X),
+		hori(Y).
 
-
-doit(2):-
-	write('Bienvenido a la opcion 2').
 
 doit(3):-
 	write('Bienvenido a la opcion 3').
